4ca862944891c87604e74a6ecb4c96ef25aa5a70
Danny van Heumen <danny@dannyvanheumen.nl>
2016-01-26 23:02:34 +0100

    Improved OTR fragment parameter verification.
    * Verify k, n values < 0.
    * Verify k, n values > 65535.
    * Verify non-empty fragment payload.
    * Refactored verification of empty trailing data.
    * Tests included.

--- src/main/java/net/java/otr4j/session/OtrAssembler.java
+++ src/main/java/net/java/otr4j/session/OtrAssembler.java
@@ -16,6 +16,8 @@ import java.net.ProtocolException;
  */
 public final class OtrAssembler {
 
+    private static final int MAX_FRAGMENTS = 65535;
+
 	/**
 	 * Accumulated fragment thus far.
 	 */
@@ -42,7 +44,7 @@ public final class OtrAssembler {
 	private static final String HEAD_FRAGMENT_V2 = "?OTR,";
 	private static final String HEAD_FRAGMENT_V3 = "?OTR|";
 
-	public OtrAssembler(InstanceTag ownInstance) {
+	public OtrAssembler(final InstanceTag ownInstance) {
 		this.ownInstance = ownInstance;
 		discard();
 	}
@@ -140,9 +142,9 @@ public final class OtrAssembler {
 			throw new ProtocolException("Expected at least 2 parameters");
 		}
 
-        // FIXME verify maximum value of 65535 for k, n
-        // FIXME disallow values < 0 too!
-		if (k == 0 || n == 0 || k > n || params.length != 4 || params[3].length() != 0) {
+		if (k <= 0 || k > MAX_FRAGMENTS || n <= 0 || n > MAX_FRAGMENTS || k > n
+                || params.length != 4 || params[2].isEmpty()
+                || !params[3].isEmpty()) {
 			discard();
 			throw new ProtocolException("Expected exactly 4 parameters and parameters according to specification");
 		}
